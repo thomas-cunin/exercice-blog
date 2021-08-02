41a04dfee3b4c106fb64354a9db424b709d5eeba
Jean-thiebaut <78347608+JeanThiebaut@users.noreply.github.com>
2021-08-02 11:06:14 +0200

    modif php formulaire commentaire

--- form_comment.php
+++ form_comment.php
@@ -1,6 +1,55 @@
 <?php
 
+if (! isset($_SESSION['auth'])) {
+	redirect('./login.php');
+}
 
+if (empty($_POST)) {
+
+    // Récupération de l'user (dont l'id est dans la SESSION) pour pré-remplir le champs Input
+    $query = $db->prepare("
+		SELECT username
+		FROM users
+		WHERE usermane = ?
+	");
+
+	$query->execute([
+		$_SESSION['username']
+	]);
+
+	$user = $query->fetch();
+
+    // Récupération de l'user (dont l'id est dans la SESSION) pour pré-remplir le champs Input
+    $query = $db->prepare("
+		SELECT id_post
+		FROM posts
+		WHERE id_post = ?
+	");
+
+	$query->execute([
+		$_GET['id_post']
+	]);
+
+	$idPost = $query->fetch();
+}
+else{
+    // Modification en base de données : récupérer toutes les données du formulaire
+	$query = $db->prepare("
+    UPDATE comments SET content = ?, id_post = ?, id_user = ?
+");
+
+$query->execute([
+    $_POST['username'],
+    $_POST['id_post'],
+    $_POST['id_user']
+]);
+
+    // Rediriger vers la liste des employés
+	redirect('./post.php?id_post');
+}
+
+
+var_dump($_POST);
 
 
 require './form_comment.phtml';
